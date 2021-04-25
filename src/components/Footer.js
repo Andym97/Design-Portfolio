b4087b90241fa68c5f965d72cbdfa0604eaaa77b
Andym97 <64376983+Andym97@users.noreply.github.com>
2021-04-25 13:29:28 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <img src= '/images/linkedin.png' className='social-link'/>
+        </div>
+    )
+}
+
+export default Footer
